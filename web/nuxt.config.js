a3d9d3f60d8e35b33f7d0e00787dc06a0e826ad2
Jorge Castellon <jorge.castellon.jr@gmail.com>
2021-07-05 01:00:36 -0700

    real v1.2.1

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -41,7 +41,7 @@ export default {
 	},
 	env: {
 		SANITY_PROJECT_ID: process.env.NUXT_PUBLIC_SANITY_PROJECT_ID,
-		VERSION_NUMBER: "1.2.0",
+		VERSION_NUMBER: "1.2.1",
 	},
 	router: {
 		middleware: ["vuex"],
