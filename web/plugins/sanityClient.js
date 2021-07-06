4a072084fde08e81194ef8b6fd267745129f7fee
Jorge Castellon <jorge.castellon.jr@gmail.com>
2021-07-05 21:10:34 -0700

    sanity cdn

--- web/plugins/sanityClient.js
+++ web/plugins/sanityClient.js
@@ -4,7 +4,7 @@ const client = sanityClient({
 	// Find your project ID and dataset in `sanity.json` in your studio project
 	projectId: process.env.SANITY_PROJECT_ID,
 	dataset: "production",
-	useCdn: false,
+	useCdn: true,
 	// useCdn == true gives fast, cheap responses using a globally distributed cache.
 	// Set this to false if your application require the freshest possible
 	// data always (potentially slightly slower and a bit more expensive).
