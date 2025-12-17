bcece4bdb4d933f8826d8a1459c8ea75a5b5fef6
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-12-17 14:42:41 -0800

    Creates migration files

--- apps/tough/src/migrations/index.ts
+++ apps/tough/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251214_215349 from './20251214_215349';
 import * as migration_20251216_200837 from './20251216_200837';
+import * as migration_20251217_224216 from './20251217_224216';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251216_200837.up,
     down: migration_20251216_200837.down,
-    name: '20251216_200837'
+    name: '20251216_200837',
+  },
+  {
+    up: migration_20251217_224216.up,
+    down: migration_20251217_224216.down,
+    name: '20251217_224216'
   },
 ];
